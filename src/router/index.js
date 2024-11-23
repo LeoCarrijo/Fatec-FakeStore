47dfc424c2211d0aafbbe075e3534977413932d1
Leonardo Carrijo <lh83712@gmail.com>
2024-11-23 20:03:54 -0300

    :tada: Criação do projeto

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: () => import('../views/ProductsView.vue'),
+    },
+    {
+      path: '/products/categories',
+      name: 'categories',
+      component: () => import('../views/CategoriesView.vue'),
+    },
+    {
+      path: '/carts',
+      name: 'carts',
+      component: () => import('../views/CartsView.vue'),
+    },
+    {
+      path: '/users',
+      name: 'users',
+      component: () => import('../views/UsersView.vue'),
+    },
+  ],
+})
+
+export default router
---
+++ src/views/UsersView.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Users</h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
