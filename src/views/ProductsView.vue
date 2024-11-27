db4a5742e03160b69ddaacfef35fc043f0c0f326
LeoCarrijo <lh83712@gmail.com>
2024-11-27 10:01:21 -0300

    :sparkles: feat: Criando sistema da página de produtos

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,14 +1,15 @@
 <template>
     <h1>Products</h1>
     <ul>
-        <li v-for="produto in produtos" :key="produto.id">
-            <span>Nome: {{ produto.title }}</span>
-            <span>Valor: {{ produto.price }}</span>
-            <span>Categoria: {{ produto.category }}</span>
-            <img :src="produto.image" >
+        <li v-for="prod in produto" :key="prod.id" @click="expandedItem(prod)">
+            <span>{{ prod.title }}</span>
+            <div class="prod__detalhes" v-show="prod.expanded">
+                <span>{{ prod.category }}</span>
+                <span>{{ prod.description }}</span>
+                <span>{{ prod.price }}</span>
+            </div>
         </li>
     </ul>
-    
 </template>
 
 <script setup>
@@ -16,9 +17,17 @@ import { ref, onMounted } from 'vue'
 
 const produto = ref(null)
 
+function expandedItem(item) {
+    item.expanded = !item.expanded 
+}
+
 async function fetchData() {
     const res = await fetch('https://fakestoreapi.com/products')
-    produto.value = await res.json()
+    const data = await res.json()
+    produto.value = data.map(item => ({
+        ...item,
+        expanded: false
+    }))
     return produto.value
 }
 
@@ -28,5 +37,19 @@ onMounted(() => {
 </script>
 
 <style scoped>
+li {
+    cursor: pointer;
+    margin-bottom: 10px;
+}
 
+li > div {
+    margin-top: 5px;
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+}
+
+.prod__detalhes {
+    background-color: red;
+}
 </style>
